1f4af160ef130fe6dfb8d42c0f7f906c95bd76aa
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-07 22:51:04 -0300

    Deleted backend

--- server/user/UserAulas.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require("sequelize");
-const database = require("../db");
-
-const UserAulas = database.define('user_aulas', {
-    userId: {
-        type: DataTypes.INTEGER(20),
-      },
-})
-module.exports = { UserAulas };
