50b0af46ecdd709744f17526f0550506b889d835
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-07 22:08:51 -0300

    Test Backend

--- client/src/Utils/Axios.js
+++ client/src/Utils/Axios.js
@@ -5,7 +5,7 @@ export default function Axios() {
   const { value } = store.getState().Auth;
 
   const http = axios.create({
-    baseURL: "http://localhost:3001/",
+    baseURL: ":3001/",
     //baseURL: "http://192.168.0.117:3001/",
 
     headers: {
