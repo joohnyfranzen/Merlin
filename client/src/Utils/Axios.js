9b60968eb79294d5e9ee84ea7fd5590a071ef3d9
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-08 17:18:46 -0300

    NGROK

--- client/src/Utils/Axios.js
+++ client/src/Utils/Axios.js
@@ -5,7 +5,7 @@ export default function Axios() {
   const { value } = store.getState().Auth;
 
   const http = axios.create({
-    baseURL: "https://26a0-2804-14c-5fe9-816a-8f3-4b46-4f33-41d4.sa.ngrok.io/",
+    baseURL: "https://ba7c-2804-14c-5fe9-816a-8f3-4b46-4f33-41d4.sa.ngrok.io/",
     //baseURL: "http://192.168.0.117:3001/",
     headers: {
       "Content-type": "application/json;charset=utf-8",
