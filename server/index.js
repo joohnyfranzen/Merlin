384a0fa650b4cd9fe0c935a5bdd90f4138aaa302
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-07 22:18:49 -0300

    Test port backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const db = require("./db");
