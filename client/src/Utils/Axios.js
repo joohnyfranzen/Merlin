fa70df929dd41550c00b214e6801f602e73c8574
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-08 16:07:24 -0300

    asdasdas

--- client/src/Utils/Axios.js
+++ client/src/Utils/Axios.js
@@ -3,14 +3,16 @@ import { store } from "..";
 
 export default function Axios() {
   const { value } = store.getState().Auth;
-  axios.defaults.baseURL =
-    "https://26a0-2804-14c-5fe9-816a-8f3-4b46-4f33-41d4.sa.ngrok.io/";
+
   axios.defaults.headers.post["Content-Type"] =
     "application/json;charset=utf-8";
   axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
   const http = axios.create({
+    baseURL =
+    "https://26a0-2804-14c-5fe9-816a-8f3-4b46-4f33-41d4.sa.ngrok.io/";
     //baseURL: "http://192.168.0.117:3001/",
+    
     headers: {
       "Content-type": "application/json",
       "Access-Control-Allow-Origin": "*",
