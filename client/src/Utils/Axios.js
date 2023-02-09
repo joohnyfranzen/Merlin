3669b39cdd2cea0b22dc5a26f152eba6d8197ac2
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-09 00:59:27 -0300

    CHATGPT TEST

--- client/src/Utils/Axios.js
+++ client/src/Utils/Axios.js
@@ -6,11 +6,7 @@ export default function Axios() {
 
   const http = axios.create({
     baseURL: "https://6ec0-2804-15e4-8008-1400-8495-8ef6-82e5-1a26.sa.ngrok.io",
-    //baseURL: "http://192.168.0.117:3001/",
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTIONS",
       "Content-Type": "application/json",
     },
   });
