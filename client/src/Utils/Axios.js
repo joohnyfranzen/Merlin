c8feba6c46ff73d2065f96893e2c8de85d1146d4
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-09 04:53:40 -0300

    AAAAAAAAEEEEEEEEEEEEEE

--- client/src/Utils/Axios.js
+++ client/src/Utils/Axios.js
@@ -5,7 +5,7 @@ export default function Axios() {
   const { value } = store.getState().Auth;
 
   const http = axios.create({
-    baseURL: "https://2a7e-2804-15e4-8008-1400-8495-8ef6-82e5-1a26.sa.ngrok.io",
+    baseURL: "https://merlin-api.vercel.app",
     headers: {
       "Content-Type": "application/json",
     },
