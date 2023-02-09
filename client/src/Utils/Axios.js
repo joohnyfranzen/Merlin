0db3c338dbd8c2cf2c2f3ff08d61d680110c25ba
Jonathan Franzen Fagundes <joohny_2014@hotmail.com>
2023-02-08 22:21:25 -0300

    Test

--- client/src/Utils/Axios.js
+++ client/src/Utils/Axios.js
@@ -4,11 +4,16 @@ import { store } from "..";
 export default function Axios() {
   const { value } = store.getState().Auth;
 
+  axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+  axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+  axios.defaults.withCredentials = true;
+
   const http = axios.create({
     baseURL: "https://ba7c-2804-14c-5fe9-816a-8f3-4b46-4f33-41d4.sa.ngrok.io/",
     //baseURL: "http://192.168.0.117:3001/",
+
     headers: {
-      "Content-type": "application/json;charset=utf-8",
+      "content-type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "*",
       "x-access-token": `${value}`,
